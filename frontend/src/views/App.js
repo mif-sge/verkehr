990ebf10d4705df87ac9032ced3fddd4b33db368
Jan Hendrik <jani.09@freenet.de>
2019-05-10 22:27:24 +0200

    issue #3, add sidebar with material ui styling

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -1,64 +1,110 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { useState } from 'react';
 
 import '../assest/css/App.css';
 
-import routes from '../routes/routes';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { HomeOutlined, MapOutlined, ChevronLeft, DirectionsOutlined } from '@material-ui/icons';
 
-/**
- * creates the application
- */
-function App() {
+const styles = {
+  icon: {
+    fontSize: 32,
+  },
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    justifyContent: 'flex-end',
+  },
+};
 
-  // structure of DOM-tree
+function App(props) {
+  const { classes } = props;
+
+  const [open, setOpen] = useState(true);
+
+  function toggleDrawer(open) {
+    setOpen(open);
+  }
+
   return (
-    < Router >
-      <div style={{ display: "flex" }}>
-        {/* sidebar div */}
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" onClick={() => toggleDrawer(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={open} onClose={() => toggleDrawer(false)}>
         <div
-          style={{
-            padding: "10px",
-            width: "40%",
-            background: "#f0f0f0"
-          }}
+          tabIndex={0}
+          role="button"
+          onClick={() => toggleDrawer(false)}
+          onKeyDown={() => toggleDrawer(false)}
         >
-          <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/map">Map</Link>
-            </li>
-            <li>
-              <Link to="/plan">Plan</Link>
-            </li>
-          </ul>
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={() => toggleDrawer(false)}>
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+                Schließen
+              </Typography>&nbsp;&nbsp;
+              <ChevronLeft />
+            </IconButton>
+          </div>
+          <Divider />
 
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.sidebar}
-            />
-          ))}
+          <List className={classes.list}>
+            <ListItem button>
+              <ListItemIcon><HomeOutlined /></ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><MapOutlined /></ListItemIcon>
+              <ListItemText primary="Map" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><DirectionsOutlined /></ListItemIcon>
+              <ListItemText primary="Plan" />
+            </ListItem>
+          </List>
         </div>
-
-        {/* main div */}
-        <div style={{ flex: 1, padding: "10px" }}>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.component}
-            />
-          ))}
-        </div>
-      </div>
-    </Router >
-
+      </Drawer>
+    </div>
   );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
