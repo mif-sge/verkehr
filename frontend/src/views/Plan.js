d488d3921a7e55e46cf54d8c4ae012cc95141270
Jan Hendrik <jani.09@freenet.de>
2019-05-10 20:06:58 +0200

    issue #2, add tube map

--- frontend/src/views/Plan.js
+++ frontend/src/views/Plan.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import * as d3 from 'd3';
+import * as tubeMap from "d3-tube-map";
+import tubeData from '../tubedata/tubedata';
 
 function Plan() {
+
+    //gets called at page load once
+    useEffect(() => {
+
+        var container = d3.select('#tube-map');
+
+        var width = 1600;
+        var height = 800;
+
+        var map = tubeMap.tubeMap()
+            .width(width)
+            .height(height)
+
+        container.datum(tubeData).call(map);
+    });
+
     return (
-        <h2>Plan</h2>
+        <div id="tube-map" style={{ height: "900px" }}>
+        </div>
     );
 }
 
