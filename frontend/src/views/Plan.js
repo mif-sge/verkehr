bb52e471e5ca40dea3ac9a98148bd94c65725afe
Jan Hendrik <=>
2019-05-10 12:49:02 +0200

    issue #23, add first fetch request to server

--- frontend/src/views/Plan.js
+++ frontend/src/views/Plan.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { DEMO } from '../urls/urls';
+
+import Button from "@material-ui/core/Button";
 
 function Plan() {
+
+    const [response, setResponse] = useState("");
+
+    function buttonClicked() {
+        fetch(DEMO, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }).then(response => setResponse(response))
+            .catch(error => {
+                setResponse(JSON.stringify(error));
+                console.error(error, "THIS SHIT FAILED!");
+            });
+    }
+
     return (
-        <h2>Plan</h2>
+        <div>
+            <h2>Plan</h2>
+            <Button simple onClick={buttonClicked}>Test</Button>
+            <p>Response:{response}</p>
+        </div>
     );
 }
 
