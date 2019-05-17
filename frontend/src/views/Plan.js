6438ff7c739b6a898354e6f4e04b6ef04c369cf2
AlexJ2017 <alexander.jaksties@gmx.de>
2019-05-17 15:50:28 +0200

    save current state before merge

--- frontend/src/views/Plan.js
+++ frontend/src/views/Plan.js
@@ -1,18 +1,24 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
 import * as d3 from 'd3';
 import * as tubeMap from "d3-tube-map";
 import tubeData from '../tubedata/tubedata';
+import style from '../assest/styles/PlanStyle';
 
-function Plan() {
+function Plan(props) {
+
+    const { classes } = props;
 
     //gets called at page load once
     useEffect(() => {
 
         var container = d3.select('#tube-map');
 
-        var width = 1600;
-        var height = 800;
+        var width = document.getElementById('tube-map').clientWidth;
+        var height = document.getElementById('tube-map').clientHeight;
+        console.log(width, "  ", height)
 
         var map = tubeMap.tubeMap()
             .width(width)
@@ -22,9 +28,13 @@ function Plan() {
     });
 
     return (
-        <div id="tube-map" style={{ height: "900px" }}>
+        <div id="tube-map" className={classes.map}>
         </div>
     );
 }
 
-export default Plan;
+Plan.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(style)(Plan);
