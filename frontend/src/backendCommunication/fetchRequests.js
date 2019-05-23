4ccec68c0ac931d1562ed455b5c195e11632d5e9
Jan Hendrik <jani.09@freenet.de>
2019-05-23 18:04:30 +0200

    issue #38, refactor shortly added snackbar code

--- frontend/src/backendCommunication/fetchRequests.js
+++ frontend/src/backendCommunication/fetchRequests.js
@@ -13,7 +13,7 @@ export async function calculateRoute() {
 
 export function fetchBusstops() {
     //TODO fetch bus stops
-
+    return []
     return [
         {
             id: 1,
--- frontend/src/components/SnackbarContentWrapper.js
+++ frontend/src/components/SnackbarContentWrapper.js
@@ -1,55 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-
 import classNames from 'classnames';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import ErrorIcon from '@material-ui/icons/Error';
-import InfoIcon from '@material-ui/icons/Info';
-import CloseIcon from '@material-ui/icons/Close';
-import green from '@material-ui/core/colors/green';
-import amber from '@material-ui/core/colors/amber';
-import IconButton from '@material-ui/core/IconButton';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import WarningIcon from '@material-ui/icons/Warning';
-import { Refresh } from '@material-ui/icons';
 
-const variantIcon = {
-  success: CheckCircleIcon,
-  warning: WarningIcon,
-  error: ErrorIcon,
-  info: InfoIcon,
-  refresh: ErrorIcon,
-};
+import { style, variantIcon } from '../assest/styles/SnackBarContentWrapperStyle';
 
-const styles1 = theme => ({
-  success: {
-    backgroundColor: green[600],
-  },
-  error: {
-    backgroundColor: theme.palette.error.dark,
-  },
-  info: {
-    backgroundColor: theme.palette.primary.dark,
-  },
-  warning: {
-    backgroundColor: amber[700],
-  },
-  refresh: {
-    backgroundColor: theme.palette.error.dark,
-  },
-  icon: {
-    fontSize: 20,
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: theme.spacing.unit,
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-});
+import { SnackbarContent, IconButton } from '@material-ui/core/';
+import { Refresh, Close } from '@material-ui/icons';
 
 function MySnackbarContent(props) {
   const { classes, className, message, onClose, variant, ...other } = props;
@@ -66,15 +23,10 @@ function MySnackbarContent(props) {
         </span>
       }
       action={[
-        <IconButton
-          key="close"
-          aria-label="Close"
-          color="inherit"
-          className={classes.close}
-          onClick={onClose}
-        >
-          {classes[variant].includes("refresh") ? <Refresh className={classes.icon} /> :
-            <CloseIcon className={classes.icon} />}
+        <IconButton key="close" aria-label="Close" color="inherit" className={classes.close} onClick={onClose}>
+          {classes[variant].includes("refresh") ?
+            <Refresh className={classes.icon} /> :
+            <Close className={classes.icon} />}
         </IconButton>,
       ]}
       {...other}
@@ -90,4 +42,4 @@ MySnackbarContent.propTypes = {
   variant: PropTypes.oneOf(['success', 'warning', 'error', 'info', 'refresh']).isRequired,
 };
 
-export default withStyles(styles1)(MySnackbarContent);
+export default withStyles(style)(MySnackbarContent);
