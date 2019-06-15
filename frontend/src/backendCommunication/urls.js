de8aedf68715ccbcb514b204684a0205b47fd4c6
AlexJ2017 <alexander.jaksties@gmx.de>
2019-06-15 15:24:07 +0200

    add current working state

--- frontend/src/backendCommunication/urls.js
+++ frontend/src/backendCommunication/urls.js
@@ -1,5 +1,5 @@
-const API = `/api`; // Production
-//const API = `http://localhost:80/api`; // Development
+//const API = `/api`; // Production
+const API = `http://localhost:80/api`; // Development
 const BUSSTOPS = `/bus/stops?requestMode=`;
 export const SHORTBUSSTOPS = `${API}${BUSSTOPS}short`;
 export const EXTENDEDBUSSTOPS = `${API}${BUSSTOPS}extended`;
--- frontend/src/views/StreetMap.js
+++ frontend/src/views/StreetMap.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import style from '../assest/styles/StreetMapStyle';
 
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer, Polyline } from 'react-leaflet'
+import { fetchBuslines } from '../backendCommunication/fetchRequests';
 
 //FH Bielefeld, Campus Minden
 const position = [52.2965164, 8.9057191];
+
 
 /**
  * creates the Open Street Map
  */
 function StreetMap(props) {
     const { classes } = props;
+    const [buslines, setBuslines] = useState([]);
+
+    const [isSubscribed, setIsSubscribed] = useState(true);
+
+    const fetchData = useCallback(async () => {
+        const tempBuslines = await fetchBuslines();
+        if (isSubscribed) {
+            setBuslines(tempBuslines);
+        }
+    }, [isSubscribed]);
+
+    useEffect(() => {
+        fetchData();
+        return () => (setIsSubscribed(false));
+    }, [fetchData]);
+    const bla = buslines[0];
+    if (bla) console.log(bla["coordinates"]);
 
     return (
         <Map center={position} zoom={13} className={classes.map}>
@@ -24,6 +43,8 @@ function StreetMap(props) {
             <Marker position={position}>
                 <Popup>FH Bielefeld,<br />Campus Minden</Popup>
             </Marker>
+            <latLngList></latLngList>
+            {buslines ? <Polyline positions={buslines[0]["coordinates"].map(waypoint => [waypoint.lat, waypoint.lng])}></Polyline> : null}
         </Map>
     );
 }
